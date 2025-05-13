9f5fe36b766253e18a380da7bbb3fdf6cb600af7
borisrosedev <boris.rose.dev@gmail.com>
2025-05-13 12:41:56 +0200

    feat/auth-archi-uml

---
+++ app/middlewares/authMiddlewares.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+const path = require('path')
+dotenv.config({
+    path: path.join(__dirname, '../../.env')
+})
+
+module.exports = {
+
+    checkIfTokenExists(req, res, next) {
+
+        const { authorization } = req.headers
+
+        if(!authorization) {
+            return res.status(401).json({ message: "no authorisation"})
+        }
+ 
+        const token = authorization.split(" ")[1]
+
+        if(!token) {
+           return res.status(401).json({ message: "no token"}) 
+        }
+
+        req.token = token
+
+        next()
+    },
+
+    decodeToken(req, res, next) {
+        const { token } = req
+        const decodedToken = jwt.verify(token,process.env.TOKEN_PRIVATE_KEY)
+     
+        if(!decodedToken) {
+            return res.status(401).json({ message: "no decoded"}) 
+        }
+
+        req.decodedToken = decodedToken
+
+        next()
+
+    }
+
+
+}
